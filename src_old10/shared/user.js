52c62541ba1f5334e5eca304338f0edb4a4977d2
leeminhee119 <mhl98@dgu.ac.kr>
2024-02-01 14:55:03 +0900

    chore: legacy delete

--- src_old10/shared/user.js
+++
@@ -1,9 +0,0 @@
-
-export const users = [
-  {
-    username: "홍길동",
-    userId: 1,
-    ID: "nininini",
-    PW: "1234"
-  }
-]
--- src_old11/shared/user.js
+++
@@ -1,9 +0,0 @@
-
-export const users = [
-  {
-    username: "김니니",
-    userId: 1,
-    ID: "nininini",
-    PW: "1234"
-  }
-]
