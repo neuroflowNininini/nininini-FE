aea3b6a38fcd400b3bd531dc0cd57713d79993cb
이민희 <mhl98@dgu.ac.kr>
2024-02-06 17:21:31 +0900

    refactor: 헤더 상단 메뉴 상수 처리

--- src/components/common/HeaderMenu/HeaderMenu.tsx
+++ src/components/common/HeaderMenu/HeaderMenu.tsx
@@ -1,58 +1,9 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Divider from '~/components/common/Divider';
-import { paths } from '~/config/paths';
+import { MainItems, OtherItems } from './HeaderMenu.const';
 
 export default function HeaderMenu() {
-  const MainItems = [
-    {
-      label: 'NEW',
-      path: paths.category('new'),
-    },
-    {
-      label: 'BEST',
-      path: paths.category('best'),
-    },
-    {
-      label: 'Season Art',
-      path: paths.category('season-art'),
-    },
-    {
-      label: 'Sale',
-      path: paths.category('sale'),
-    },
-    {
-      label: 'Nail',
-      path: paths.category('nail'),
-    },
-    {
-      label: 'Pedi',
-      path: paths.category('pedi'),
-    },
-    {
-      label: 'etc',
-      path: paths.category('etc'),
-    },
-  ] as const;
-
-  const OtherItems = [
-    {
-      label: 'EVENT',
-      path: paths.event(),
-    },
-    {
-      label: 'REVIEW',
-      path: paths.review(),
-    },
-    {
-      label: '공지사항',
-      path: paths.notice(),
-    },
-    {
-      label: 'FAQ',
-      path: paths.faq(),
-    },
-  ];
   return (
     <MenuContainer>
       <MenuBox>
