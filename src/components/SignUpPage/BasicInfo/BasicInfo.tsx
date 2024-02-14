c9dae8bd1159e3eee218a432b432d51a819fd96f
이민희 <mhl98@dgu.ac.kr>
2024-02-14 17:13:04 +0900

    refactor: 기본정보 입력 컴포넌트 간격 조정

--- src/components/SignUpPage/BasicInfo/BasicInfo.tsx
+++ src/components/SignUpPage/BasicInfo/BasicInfo.tsx
@@ -52,102 +52,98 @@ export default function BasicInfo({ onNext }: BasicInfoProps) {
 
   return (
     <Container>
-      <TopWrap>
-        <SignUpHeader
-          totalSteps={4}
-          step={2}
-        />
-        <FormWrap>
+      <SignUpHeader
+        totalSteps={4}
+        step={2}
+      />
+      <FormWrap>
+        <InputBtnWrap>
+          <Label>아이디</Label>
           <InputBtnWrap>
-            <Label>아이디</Label>
-            <InputBtnWrap>
-              <Input
-                value={id} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setId(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
-              />
-              {/*TODO - 중복확인 기능 */}
-              <ThemeButton
-                onClick={() => {}}
-                variant="reversed"
-                width="15rem"
-                height="100%"
-                fontSize={'smallmedium'}
-                style={{ marginLeft: '3px' }}
-              >
-                중복확인
-              </ThemeButton>
-            </InputBtnWrap>
-          </InputBtnWrap>
-          <InputBtnWrap>
-            <InputBtnWrap>
-              <Label>비밀번호</Label>
-              <Input
-                value={pw} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
-                type="password"
-                onChange={handlePwChange} // 입력 필드 값이 변경될 때 핸들러 호출
-              />
-            </InputBtnWrap>
-          </InputBtnWrap>
-          <InputBtnWrap>
-            <Label>비밀번호 확인</Label>
             <Input
-              value={pwConfirm} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
+              value={id} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setId(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
+            />
+            {/*TODO - 중복확인 기능 */}
+            <ThemeButton
+              onClick={() => {}}
+              variant="reversed"
+              width="15rem"
+              height="100%"
+              fontSize={'smallmedium'}
+              style={{ marginLeft: '3px' }}
+            >
+              중복확인
+            </ThemeButton>
+          </InputBtnWrap>
+        </InputBtnWrap>
+        <InputBtnWrap>
+          <InputBtnWrap>
+            <Label>비밀번호</Label>
+            <Input
+              value={pw} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
               type="password"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPwConfirm(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
+              onChange={handlePwChange} // 입력 필드 값이 변경될 때 핸들러 호출
             />
           </InputBtnWrap>
-          <InputBtnWrap>
-            <Label>이름</Label>
-            <Input
-              value={name} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
-            />
-          </InputBtnWrap>
-          <InputBtnWrap>
-            <Label>연락처</Label>
-            <Input
-              value={phoneNumber} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhoneNumber(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
-            />
-          </InputBtnWrap>
-          <InputBtnWrap>
-            <Label>이메일</Label>
-            <Input
-              value={email} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
-            />
-          </InputBtnWrap>
-          <InputBtnWrap>
-            <Label>주민등록번호</Label>
-            <Input
-              value={birth}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBirth(e.target.value)}
-              placeholder="주민번호 앞 6자리"
-              style={{ width: '15rem' }}
-            />
-            <Divider
-              length="2rem"
-              margin="0 .7rem"
-            />
-            <Input
-              value={sex}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSexChange(e)}
-              style={{ width: '5rem' }}
-            />
-            <Text>xxxxxx</Text>
-          </InputBtnWrap>
-        </FormWrap>
-      </TopWrap>
+        </InputBtnWrap>
+        <InputBtnWrap>
+          <Label>비밀번호 확인</Label>
+          <Input
+            value={pwConfirm} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
+            type="password"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPwConfirm(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
+          />
+        </InputBtnWrap>
+        <InputBtnWrap>
+          <Label>이름</Label>
+          <Input
+            value={name} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
+          />
+        </InputBtnWrap>
+        <InputBtnWrap>
+          <Label>연락처</Label>
+          <Input
+            value={phoneNumber} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhoneNumber(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
+          />
+        </InputBtnWrap>
+        <InputBtnWrap>
+          <Label>이메일</Label>
+          <Input
+            value={email} // 입력 필드의 값은 상태 변수와 바인딩됩니다.
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} // 입력 필드 값이 변경될 때 핸들러 호출
+          />
+        </InputBtnWrap>
+        <InputBtnWrap>
+          <Label>주민등록번호</Label>
+          <Input
+            value={birth}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBirth(e.target.value)}
+            placeholder="주민번호 앞 6자리"
+            style={{ width: '15rem' }}
+          />
+          <Divider
+            length="2rem"
+            margin="0 .7rem"
+          />
+          <Input
+            value={sex}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSexChange(e)}
+            style={{ width: '5rem' }}
+          />
+          <Text>xxxxxx</Text>
+        </InputBtnWrap>
+      </FormWrap>
       <ThemeButton onClick={handleContinue}>다음</ThemeButton>
     </Container>
   );
 }
-const Container = styled.div``;
-
-const TopWrap = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 7rem;
+  gap: 5rem;
 `;
 
 const FormWrap = styled.div`
