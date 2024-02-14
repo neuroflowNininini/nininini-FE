4d7c6995b4472c41f77fed9690a73527dccde234
이민희 <mhl98@dgu.ac.kr>
2024-02-14 13:35:26 +0900

    fix: styled-component props html 속성으로 못 넘어가게 방지

--- src/components/common/Stepper/Stepper.tsx
+++ src/components/common/Stepper/Stepper.tsx
@@ -11,11 +11,11 @@ export default function Stepper({ total, step, size, gap }: StepperProps) {
   const circles = new Array(total).fill(false).map((_, i) => i + 1 <= step && true);
   return (
     <Container gap={gap}>
-      {circles.map((isFilled, index) => (
+      {circles.map((isCurrent, index) => (
         <Circle
           key={index}
-          isFilled={isFilled}
           size={size}
+          $isfilled={isCurrent}
         />
       ))}
     </Container>
@@ -26,9 +26,9 @@ const Container = styled.div<{ gap: string }>`
   display: flex;
   gap: ${({ gap }) => gap ?? '1rem'};
 `;
-const Circle = styled.div<{ isFilled: boolean; size: string }>`
-  background-color: ${({ theme, isFilled }) =>
-    isFilled ? theme.colors.gray['900'] : theme.colors.white['500']};
+const Circle = styled.div<{ $isfilled: boolean; size: string }>`
+  background-color: ${({ $isfilled, theme }) =>
+    $isfilled ? theme.colors.gray['900'] : theme.colors.white['500']};
   width: ${({ size }) => size ?? '1.3rem'};
   height: ${({ size }) => size ?? '1.3rem'};
   border-radius: 50%;
