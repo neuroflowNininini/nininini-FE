c04b828a167a083fdc9f2da1aa54ba41d7526eaf
이민희 <mhl98@dgu.ac.kr>
2024-02-16 13:40:37 +0900

    feat: 아이디 중복 검사 api 연결

--- src/api/signUp.ts
+++ src/api/signUp.ts
@@ -55,3 +55,15 @@ export const postCheckDuplicateId = async ({
     }
   }
 };
+
+export const postCheckDuplicateId = async (userId: string) => {
+  const res = await fetch(process.env.REACT_APP_API_BASE_URL + `/api/members/idcheck`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userId }),
+  });
+
+  return res.json();
+};
