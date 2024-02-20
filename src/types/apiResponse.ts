0ad9b2c587c61687aca4f19d75e91e9187b6dbf0
이민희 <mhl98@dgu.ac.kr>
2024-02-20 17:59:09 +0900

    fix: 재발급 error status code 수정

--- src/types/apiResponse.ts
+++ src/types/apiResponse.ts
@@ -1,4 +1,4 @@
-export type ErrorCode = 'INVALID_ACCESS_TOKEN' | 'INVALID_AUTHENTICATION';
+export type ErrorCode = 'INVALID_ACCESS_TOKEN' | 'INVALID_AUTHENTICATION' | 'WRONG_ID' | 'WRONG_PW';
 
 export interface NinininiResponse<T> extends Response {
   parsedBody: T | NinininiErrorResponse;
