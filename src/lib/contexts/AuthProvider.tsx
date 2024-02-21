bda9456ad8fbc57e53b1671977bb3c81f8895dbb
이민희 <mhl98@dgu.ac.kr>
2024-02-21 11:34:20 +0900

    feat: auth provider 작성, 전역 상태화

---
+++ src/lib/contexts/AuthProvider.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { NinininiAxios } from '~/config/axios';
+import { CONSTANTS } from '~/constants';
+import { getCookie } from '~/utils/cookie';
+
+const AuthContext = createContext({
+  userInfo: null,
+  isLoggedIn: false,
+  getUserInfo: () => {},
+});
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  /*TODO - user 정보 스키마 정해지면 타입 지정 */
+  const [userInfo, setUserInfo] = useState(null);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const getUserInfo = async () => {
+    try {
+      const { data } = await NinininiAxios.get(`/api/members/user`);
+      if (data) {
+        setUserInfo(data);
+        setLoggedIn(true);
+      }
+      return;
+    } catch (e) {
+      // throw new Error(e);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ userInfo, isLoggedIn, getUserInfo }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const { getUserInfo, userInfo, isLoggedIn } = useContext(AuthContext);
+  const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_KEY);
+  useEffect(() => {
+    if (accessToken) {
+      getUserInfo();
+    }
+  }, []);
+  return { userInfo, isLoggedIn };
+};
