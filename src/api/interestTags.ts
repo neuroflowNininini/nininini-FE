1ef0cfa826f959bd472dfd26c544674101785f61
이민희 <mhl98@dgu.ac.kr>
2024-03-08 17:31:07 +0900

    refactor: axios 리턴 타입 정의

--- src/api/interestTags.ts
+++ src/api/interestTags.ts
@@ -1,6 +1,9 @@
 import { NinininiAxios } from '~/config/axios';
+import { Tag } from '~/types/tag';
 
-export const getInterestTags = async () => {
-  const data = await NinininiAxios.get(`/api/tags`);
-  return data;
+export const getInterestTags = async (): Promise<Tag[]> => {
+  const {
+    data: { tags },
+  } = await NinininiAxios.get(`/api/tags`);
+  return tags;
 };
