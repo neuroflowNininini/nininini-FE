8875d78321f1c0427883423a5ea00d26ff398d97
이민희 <mhl98@dgu.ac.kr>
2024-03-14 14:43:15 +0900

    fix: device detect width로 판단

--- src/hooks/useDeviceSizeDetect.ts
+++ src/hooks/useDeviceSizeDetect.ts
@@ -2,13 +2,11 @@ import { useState, useEffect } from 'react';
 import { deviceSizes } from '~/styles/breakpoints';
 
 export const useDeviceSizeDetect = (size: keyof typeof deviceSizes = 'md') => {
-  const [withinTargetSize, setWithinTargetSize] = useState(
-    Math.min(window.innerWidth, window.innerHeight) < deviceSizes[size],
-  );
+  const [withinTargetSize, setWithinTargetSize] = useState(window.innerWidth < deviceSizes[size]);
 
   useEffect(() => {
     const detectBrowserSize = () => {
-      if (Math.min(window.innerWidth, window.innerHeight) < deviceSizes[size]) {
+      if (window.innerWidth < deviceSizes[size]) {
         setWithinTargetSize(true);
       } else {
         setWithinTargetSize(false);
