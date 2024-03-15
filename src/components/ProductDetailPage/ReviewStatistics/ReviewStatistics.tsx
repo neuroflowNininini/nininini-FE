b9c54f2111689d994d0b69915106bc6b618915bf
이민희 <mhl98@dgu.ac.kr>
2024-03-15 10:34:38 +0900

    feat: 리뷰 통계 데이터 전달, 리뷰 없는 경우 처리

--- src/components/ProductDetailPage/ReviewStatistics/ReviewStatistics.tsx
+++ src/components/ProductDetailPage/ReviewStatistics/ReviewStatistics.tsx
@@ -3,21 +3,25 @@ import { StarRate } from '~/components/common/StarRate';
 import { Text } from '~/components/common/Text';
 import ReviewBarChart from './ReviewBarChart';
 
-/*FIXME - 더미데이터 변경 */
-export default function ReviewStatistics() {
+interface ReviewStatisticsProps {
+  overallRate: number;
+  ratiosData: number[];
+}
+
+export default function ReviewStatistics({ overallRate, ratiosData }: ReviewStatisticsProps) {
   return (
     <Container>
       <Column>
         <Text>구매자 총 평점</Text>
-        <Rate>4.9</Rate>
+        <Rate>{overallRate}</Rate>
         <StarRate
-          rate={4.9}
+          rate={overallRate}
           size={20}
         />
       </Column>
       <Column>
         <Text>평점 비율</Text>
-        <ReviewBarChart ratiosData={[1, 0.5, 0, 0, 0]} />
+        <ReviewBarChart ratiosData={ratiosData} />
       </Column>
     </Container>
   );
